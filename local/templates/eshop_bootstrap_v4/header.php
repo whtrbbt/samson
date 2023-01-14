108612728a5b0c18347a6b2164abc18eeb98c528
whtrbbt <krafter2006@yandex.ru>
2023-01-14 17:18:24 +0300

    добавил обработчик сравнения в header.php, добавил компонент catalog в папку local

--- local/templates/eshop_bootstrap_v4/header.php
+++ local/templates/eshop_bootstrap_v4/header.php
@@ -205,6 +205,28 @@ $curPage = $APPLICATION->GetCurPage(true);
 			<?endif?>
 			<!--endregion-->
 		</div>
+		
+		<div class="compare_list_count">
+			<?
+				$APPLICATION->IncludeComponent(
+					"bitrix:catalog.compare.list", 
+					"", 
+					array(
+						"IBLOCK_TYPE" => "catalog", //тип инфоблока каталога
+						"IBLOCK_ID" => "2", //ID инфоблока каталога
+						"AJAX_MODE" => "N",
+						"AJAX_OPTION_JUMP" => "N",
+						"AJAX_OPTION_STYLE" => "Y",
+						"AJAX_OPTION_HISTORY" => "N",
+						"DETAIL_URL" => "#SECTION_CODE#",
+						"COMPARE_URL" => "/catalog/compare.php",
+						"NAME" => "CATALOG_COMPARE_LIST",
+						"AJAX_OPTION_ADDITIONAL" => ""
+					),
+					false
+				);
+			?>
+		</div>
 	</header>
 
 	<div class="workarea">
