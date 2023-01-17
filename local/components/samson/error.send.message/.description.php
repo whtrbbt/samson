d8dfa9aff63e167af61410937412613b4ced3d1a
whtrbbt <krafter2006@yandex.ru>
2023-01-17 08:56:34 +0300

    создал заготовку компонента

---
+++ local/components/samson/error.send.message/.description.php
@@ -0,0 +1,5 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die(); $arComponentDescription = array(
+"NAME" => GetMessage("Сообщение админам об ошибке"),
+"DESCRIPTION" => GetMessage("Позволяет выделить на сайте ошибку в тексте, нажать Ctrl+Enter и отправить её админам на почту"),
+);
+?>
